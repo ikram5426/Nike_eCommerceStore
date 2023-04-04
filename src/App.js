affa286c7f3b3597a11fbb3cc189a089259e762c
IkramKhan <ikram5426@gmail.com>
2023-04-05 01:57:11 +0500

    Stories section completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
-import { Content, Hero, Sales } from './component'
-import { heroapi, popularSales ,topRatedSales,highlight, sneaker} from "../src/data/data";
+import { Content, Hero, Sales, Stories } from './component'
+import { heroapi, popularSales ,topRatedSales,highlight, sneaker, story} from "../src/data/data";
 
 const App = () => {
   return (
-    <div className="flex flex-col gap-16 relative">
+    <main className="flex flex-col gap-16 relative">
       <Hero heroapi={heroapi}></Hero>
       <Sales endPoint={popularSales} ifExist></Sales>
       <Content endPoint={highlight} ifExist></Content>
       <Sales endPoint={topRatedSales}></Sales>
       <Content endPoint={sneaker}></Content>
-    </div>
+      <Stories story={story}></Stories>
+    </main>
   );
 }
 
--- src/component/index.js
+++ src/component/index.js
@@ -1,3 +1,4 @@
 export {default as Hero} from './Hero/Hero.jsx'
 export { default as Sales} from './Sales/Sales.jsx'
 export {default as Content} from './Content'
+export { default as Stories } from "./Stories";

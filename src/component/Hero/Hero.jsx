f0c12ee28d552a339135a4f6a7cb1cf2415ab214
IkramKhan <ikram5426@gmail.com>
2023-04-06 00:23:53 +0500

    Navbar completed

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = ({
   heroapi: { title, subtitle, btntext, img, videos, sociallinks },
 }) => {
   return (
-    <div className="relative w-auto h-auto flex flex-col">
+    <div className="relative w-auto h-auto pt-[9vh] flex flex-col">
       <div className="bg-theme clip-path h-[85vh] lg:h-[75vh] md:h-[65vh] sm:h-[55vh] w-auth top-0 left-0 absolute z-0 right-0 opacity-100"></div>
-      <div className="opacity-100 z-100 relative grid item-center justify-items-center nike-container mt-28 md:mt-24">
+      <div className="opacity-100 z-100 relative grid item-center justify-items-center nike-container mt-20 md:mt-16">
         <div>
           <h1 className="text-6xl lg:text-5xl md:text-4xl sm:text-3xl xsm:text-2xt font-extrabold filter drop-shadow-sm text-slate-200 pb-4">
             {title}
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React,{useEffect, useState} from "react";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import logo from '../assets/logo.png'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [navState , SetNavState]=useState(false)
+  const navController=()=>{
+    if(window.scrollY > 30){
+      SetNavState(true)
+    }else{
+      SetNavState(false)
+    }
+  }
 
-export default Navbar
+  useEffect(()=>{
+    window.addEventListener("scroll",navController)
+    return ()=>{
+      window.removeEventListener("scroll",navController)
+    }
+  },[])
+  return (
+    <header
+      className={
+        !navState
+          ? "absolute top-7 left-0 right-0 opacity-100 z-50"
+          : "fixed top-0 left-0 right-0 h-[9vh] flex items-center justify-center opacity-100 z-[200] blur-effect-theme"
+      }
+    >
+      <nav className="flex items-center justify-between nike-container">
+        <div className="flex items-center">
+          <img
+            src={logo}
+            alt="logo/img"
+            className={`w-16 h-auto ${navState && "filter brightness-0"}`}
+          />
+        </div>
+        <ul className="flex items-center justify-center gap-2">
+          <li className="grid items-center">
+            <SearchOutlinedIcon
+              className={`icon-style ${
+                navState && "text-slate-900 transition-all duration-300"
+              }`}
+            />
+          </li>
+          <li className="grid items-center">
+            <FavoriteIcon
+              className={`icon-style ${
+                navState && "text-slate-900 transition-all duration-300"
+              }`}
+            />
+          </li>
+          <li className="grid items-center">
+            <button
+              type="button"
+              className="border-none outline-none active:scale-110 transition-all duration-300 relative"
+            >
+              <ShoppingBagOutlinedIcon
+                className={`icon-style ${
+                  navState && "text-slate-900 transition-all duration-300"
+                }`}
+              />
+              <div
+                className={`absolute top-4 right-0 shadow w-4 h-4 text-[0.65rem] leading-tight font-medium rounded-full flex items-center justify-center cursor-pointer hover:scale-110 transition-all duration-300 ${
+                  navState
+                    ? "bg-slate-900 text-slate-100 shadow-slate-900"
+                    : "bg-slate-100 text-slate-900 shadow-slate-100"
+                }`}
+              >
+                0
+              </div>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
